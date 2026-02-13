0bee092e91e0c02aeb7a51323a25840662a1122c
Mohammad Azril Sugiarto <azrilsugiarto06@gmail.com>
2026-02-13 20:30:25 +0700

    feat: add semester and kuota fields to matkul table in migration

--- database/migrations/2025_10_11_150640_create_letter.php
+++ database/migrations/2025_10_11_150640_create_letter.php
@@ -31,6 +31,8 @@ return new class extends Migration
             $table->id();
             $table->string("kode_matkul");
             $table->string("nama");
+            $table->string("semester");
+            $table->integer("kuota");
             $table->bigInteger("prodi_id");
             $table->foreign("prodi_id")->references("id")->on("prodi");
             $table->timestamps();
